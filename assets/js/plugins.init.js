9adf04a0545e13c7df0f7c80a6338f918c5ced11
Romil Patel <160582237+Romil-Hingrajiya@users.noreply.github.com>
2024-04-09 14:16:08 +0530

    Add files via upload

---
+++ assets/js/plugins.init.js
@@ -0,0 +1,429 @@
+/* Template Name: Hously - Tailwind CSS Real Estate Template
+   Author: Shreethemes
+   Email: support@shreethemes.in
+   Website: https://shreethemes.in
+   Version: 1.5.0
+   Created: June 2022
+   File Description: Common JS file of the template(plugins.init.js)
+*/
+
+
+/*********************************/
+/*         INDEX                 */
+/*================================
+ *     01.  Tiny Slider          *
+ *     02.  Tobii lightbox       * (For Portfolio pages)
+ *     03.  Components           *
+ *          1. Navtabs           *
+ *          2. Accordions        *
+ *     04.  Typed Text animation (animation) * 
+ ================================*/
+         
+//=========================================//
+/*            01) Tiny slider              */
+//=========================================//
+
+if(document.getElementsByClassName('tiny-single-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-single-item',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 16,
+    });
+};
+
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-home-slide-three').length > 0) {
+    var slider = tns({
+        container: '.tiny-home-slide-three',
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+//=========================================//
+/*/*            02) Tobii lightbox         */
+//=========================================//
+
+try {
+    const tobii = new Tobii()
+} catch (err) {
+
+}
+
+//=========================================//
+/*            03) Components               */
+//=========================================//
+
+//============= 01) Navtabs ===============//
+try {
+    const Default = {
+        defaultTabId: null,
+        activeClasses: 'text-white bg-green-600',
+        inactiveClasses: 'hover:text-green-600 dark:hover:text-white hover:bg-gray-50 dark:hover:bg-slate-800',
+        onShow: () => { }
+    }
+    
+    class Tabs {
+        constructor(items = [], options = {}) {
+            this._items = items
+            this._activeTab = options ? this.getTab(options.defaultTabId) : null
+            this._options = { ...Default, ...options }
+            this._init()
+        }
+    
+        _init() {
+            if (this._items.length) {
+                // set the first tab as active if not set by explicitly
+                if (!this._activeTab) {
+                    this._setActiveTab(this._items[0])
+                }
+    
+                // force show the first default tab
+                this.show(this._activeTab.id, true)
+    
+                // show tab content based on click
+                this._items.map(tab => {
+                    tab.triggerEl.addEventListener('click', () => {
+                        this.show(tab.id)
+                    })
+                })
+            }
+        }
+    
+        getActiveTab() {
+            return this._activeTab
+        }
+    
+        _setActiveTab(tab) {
+            this._activeTab = tab
+        }
+    
+        getTab(id) {
+            return this._items.filter(t => t.id === id)[0]
+        }
+    
+        show(id, forceShow = false) {
+            const tab = this.getTab(id)
+    
+            // don't do anything if already active
+            if (tab === this._activeTab && !forceShow) {
+                return
+            }
+    
+            // hide other tabs
+            this._items.map(t => {
+                if (t !== tab) {
+                    t.triggerEl.classList.remove(...this._options.activeClasses.split(" "));
+                    t.triggerEl.classList.add(...this._options.inactiveClasses.split(" "));
+                    t.targetEl.classList.add('hidden')
+                    t.triggerEl.setAttribute('aria-selected', false)
+                }
+            })
+    
+            // show active tab
+            tab.triggerEl.classList.add(...this._options.activeClasses.split(" "));
+            tab.triggerEl.classList.remove(...this._options.inactiveClasses.split(" "));
+            tab.triggerEl.setAttribute('aria-selected', true)
+            tab.targetEl.classList.remove('hidden')
+    
+            this._setActiveTab(tab)
+    
+            // callback function
+            this._options.onShow(this, tab)
+        }
+    
+    }
+    
+    window.Tabs = Tabs;
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('[data-tabs-toggle]').forEach(triggerEl => {
+    
+            const tabElements = []
+            let defaultTabId = null
+            triggerEl.querySelectorAll('[role="tab"]').forEach(el => {
+                const isActive = el.getAttribute('aria-selected') === 'true'
+                const tab = {
+                    id: el.getAttribute('data-tabs-target'),
+                    triggerEl: el,
+                    targetEl: document.querySelector(el.getAttribute('data-tabs-target'))
+                }
+                tabElements.push(tab)
+    
+                if (isActive) {
+                    defaultTabId = tab.id
+                }
+            })
+            new Tabs(tabElements, {
+                defaultTabId: defaultTabId
+            })
+        })
+    })
+} catch (error) {
+    
+}
+
+//********2) Accordions********/
+try {
+    const Default = {
+        alwaysOpen: false,
+        activeClasses: 'bg-gray-50 dark:bg-slate-800 text-green-600',
+        inactiveClasses: 'text-dark dark:text-white',
+        onOpen: () => { },
+        onClose: () => { },
+        onToggle: () => { }
+    }
+    
+    class Accordion {
+        constructor(items = [], options = {}) {
+            this._items = items
+            this._options = { ...Default, ...options }
+            this._init()
+        }
+    
+        _init() {
+            if (this._items.length) {
+                // show accordion item based on click
+                this._items.map(item => {
+    
+                    if (item.active) {
+                        this.open(item.id)
+                    }
+    
+                    item.triggerEl.addEventListener('click', () => {
+                        this.toggle(item.id)
+                    })
+                })
+            }
+        }
+    
+        getItem(id) {
+            return this._items.filter(item => item.id === id)[0]
+        }
+    
+        open(id) {
+            const item = this.getItem(id)
+    
+            // don't hide other accordions if always open
+            if (!this._options.alwaysOpen) {
+                this._items.map(i => {
+                    if (i !== item) {
+                        i.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
+                        i.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
+                        i.targetEl.classList.add('hidden')
+                        i.triggerEl.setAttribute('aria-expanded', false)
+                        i.active = false
+    
+                        // rotate icon if set
+                        if (i.iconEl) {
+                            i.iconEl.classList.remove('rotate-180')
+                        }
+                    }
+                })
+            }
+    
+            // show active item
+            item.triggerEl.classList.add(...this._options.activeClasses.split(" "))
+            item.triggerEl.classList.remove(...this._options.inactiveClasses.split(" "))
+            item.triggerEl.setAttribute('aria-expanded', true)
+            item.targetEl.classList.remove('hidden')
+            item.active = true
+    
+            // rotate icon if set
+            if (item.iconEl) {
+                item.iconEl.classList.add('rotate-180')
+            }
+    
+            // callback function
+            this._options.onOpen(this, item)
+        }
+    
+        toggle(id) {
+            const item = this.getItem(id)
+    
+            if (item.active) {
+                this.close(id)
+            } else {
+                this.open(id)
+            }
+    
+            // callback function
+            this._options.onToggle(this, item)
+        }
+    
+        close(id) {
+            const item = this.getItem(id)
+    
+            item.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
+            item.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
+            item.targetEl.classList.add('hidden')
+            item.triggerEl.setAttribute('aria-expanded', false)
+            item.active = false
+    
+            // rotate icon if set
+            if (item.iconEl) {
+                item.iconEl.classList.remove('rotate-180')
+            }
+    
+            // callback function
+            this._options.onClose(this, item)
+        }
+    
+    }
+    
+    window.Accordion = Accordion;
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('[data-accordion]').forEach(accordionEl => {
+    
+            const alwaysOpen = accordionEl.getAttribute('data-accordion')
+            const activeClasses = accordionEl.getAttribute('data-active-classes')
+            const inactiveClasses = accordionEl.getAttribute('data-inactive-classes')
+    
+            const items = []
+            accordionEl.querySelectorAll('[data-accordion-target]').forEach(el => {
+                const item = {
+                    id: el.getAttribute('data-accordion-target'),
+                    triggerEl: el,
+                    targetEl: document.querySelector(el.getAttribute('data-accordion-target')),
+                    iconEl: el.querySelector('[data-accordion-icon]'),
+                    active: el.getAttribute('aria-expanded') === 'true' ? true : false
+                }
+                items.push(item)
+            })
+    
+            new Accordion(items, {
+                alwaysOpen: alwaysOpen === 'open' ? true : false,
+                activeClasses: activeClasses ? activeClasses : Default.activeClasses,
+                inactiveClasses: inactiveClasses ? inactiveClasses : Default.inactiveClasses
+            })
+        })
+    })
+} catch (error) {
+    
+}
+
+
+//=========================================//
+/*/* 4) Typed Text animation (animation) */
+//=========================================//
+
+try {
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+        if (this.isDeleting) { delta /= 2; }
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    function typewrite() {
+        if (toRotate === 'undefined') {
+            changeText()
+        }
+        else
+            var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid transparent}";
+        document.body.appendChild(css);
+    };
+    window.onload(typewrite());
+} catch(err) {
+
+}
